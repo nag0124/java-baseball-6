8cb407bd531f20ab9f0a74df318e96b349361602
nag0124 <wlsdnjs0124@gmail.com>
2023-10-20 23:52:08 +0900

    feat: 시작 메세지 출력 메서드 삭제
    - 프로그램시 한번만 출력되기에 메인메소드에 적으려고 합니다.

--- src/main/java/baseball/Screen.java
+++ src/main/java/baseball/Screen.java
@@ -18,8 +18,4 @@ public class Screen {
         System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
     }
 
-    public void printStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
 }
