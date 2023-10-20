76f2f9f9b38f4bde6f0bc98ad945c147270d1e76
nag0124 <wlsdnjs0124@gmail.com>
2023-10-20 23:16:55 +0900

    feat: 게임 결과를 출력하는 Screen 클래스 작성

---
+++ src/main/java/baseball/Screen.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import java.util.List;
+
+public class Screen {
+
+    public void print(List<Integer> gameResult) {
+        int strikeCount = gameResult.get(0);
+        int ballCount = gameResult.get(1);
+
+        if (strikeCount == 3 && ballCount == 0) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
+        System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+    }
+
+}
