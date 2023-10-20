b92757f7c4eea3d74530712cd267d6fcb29d8440
nag0124 <wlsdnjs0124@gmail.com>
2023-10-21 00:04:46 +0900

    feat: 숫자 입력 메세지 출력 구현

--- src/main/java/baseball/NumberScanner.java
+++ src/main/java/baseball/NumberScanner.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class NumberScanner {
 
     public int input() {
+        System.out.print("숫자를 입력해주세요 : ");
         int number = Integer.parseInt(Console.readLine());
 
         validate(number);
