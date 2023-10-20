6d9ce9941ecd7594109a4b2a643a659d76dbd5ab
nag0124 <wlsdnjs0124@gmail.com>
2023-10-21 00:06:22 +0900

    fix: 어플리케이션 클래스 구현
    - 게임을 새로 시작 여부를 입력받는 베서드도 구현했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,32 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        int readLine = Integer.parseInt(Console.readLine());
-        System.out.println("readLine = " + readLine);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        GameManager manager = new GameManager();
+        boolean doesPlayerWantGame = true;
+
+        while (doesPlayerWantGame) {
+            manager.playOneGame();
+            doesPlayerWantGame = checkPlayerWantsGame();
+        }
+    }
+
+    private static boolean checkPlayerWantsGame() {
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int reply = Integer.parseInt(Console.readLine());
+
+            if (reply == 1) {
+                return true;
+            }
+            if (reply == 2) {
+                return false;
+            }
+        }
     }
 }
