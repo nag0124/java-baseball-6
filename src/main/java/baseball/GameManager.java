b3212ff92ea16e5cf28632f310c485a62ff737a4
nag0124 <wlsdnjs0124@gmail.com>
2023-10-20 23:51:09 +0900

    feat: 게임 진행을 담당하는 GameManager 클래스 구현
    - 정답을 맞출 때 까지 게임을 진행합니다.

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import java.util.List;
+
+public class GameManager {
+
+    private final NumberScanner scanner = new NumberScanner();
+    private final RandomBaseballNumberGenerator generator = new RandomBaseballNumberGenerator();
+    private final GameJudge judge = new GameJudge();
+    private final Screen screen = new Screen();
+
+    public void playOneGame() {
+        int computerNumber = generator.generate();
+        boolean hasNextRound = true;
+
+        while (hasNextRound) {
+            int playerNumber = scanner.input();
+            List<Integer> result = judge.judge(playerNumber, computerNumber);
+            screen.printResult(result);
+            hasNextRound = checkNextRound(result);
+        }
+    }
+
+    private boolean checkNextRound(List<Integer> gameResult) {
+        if (gameResult.get(0) == 3 && gameResult.get(1) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
